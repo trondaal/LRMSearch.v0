8605ed5fa4cce2988be02d3b035a5c7939a75786
Trond Aalberg <trond.aalberg@ntnu.no>
2022-01-20 10:18:41 +0100

    New version of dataset

--- dataset/neo4j-import/rename.sh
+++ dataset/neo4j-import/rename.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-mv query-result-5.srj agentrels.json 
-mv query-result-2.srj concepts.json     
-mv query-result-7.srj partofrels.json   
-mv query-result-3.srj types.json 
-mv query-result-4.srj basicrels.json    
 mv query-result.srj   entities.json 
-mv query-result-9.srj properties.json
+mv query-result-2.srj concepts.json     
+mv query-result-3.srj types.json 
+mv query-result-4.srj basicrels.json  
+mv query-result-5.srj agentrels.json   
 mv query-result-6.srj subjectrels.json   
+mv query-result-7.srj partofrels.json   
 mv query-result-8.srj workrels.json 
+mv query-result-9.srj properties.json
