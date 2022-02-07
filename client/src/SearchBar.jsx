21b4251d974a32bdc92b4f01fc8c135da728c1e3
Trond Aalberg <trond.aalberg@ntnu.no>
2022-02-07 11:56:03 +0100

    stopword and sessionstorage
    Queries are now stored in sessionsstorage and reused upon reload.
    Also added a stopwordlist and filtering out stopwords before querying.

--- client/src/SearchBar.jsx
+++ client/src/SearchBar.jsx
@@ -2,20 +2,36 @@
 import React, {useState, useMemo, useEffect} from 'react';
 import TextField from '@mui/material/TextField';
 import {selectedVar} from './api/Cache';
+import stopwords from './Search/stopwords'
 
 
 
 export default function SearchBar(props) {
-    const [query, setQuery] = useState("Harry Potter");
+
+
+    const [query, setQuery] = useState(sessionStorage.getItem('query') ? sessionStorage.getItem('query') : "Harry Potter");
 
     const changeHandler = (event) => {
         if (event.key === 'Enter') {
             selectedVar(new Set());
-            props.search({ variables: { query: query.split(" ").join(" AND ") } })
+            sessionStorage.setItem('query', query);
+            props.search({ variables: { query: query.split(" ").filter((word) => !stopwords.includes(word.toLowerCase())).join(" AND ") } })
         }else{
             setQuery(event.target.value)
         }
     };
+
+    useEffect(() => {
+        if (sessionStorage.getItem('query') === null){
+            sessionStorage.setItem('query', query);
+        }else{
+            setQuery(sessionStorage.getItem('query'))
+        }
+        props.search({ variables: { query: query.split(" ").join(" AND ") } });
+        }, []
+
+    );
+
     return <TextField
         id="filled-search"
         fullWidth
